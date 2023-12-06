fda3b212eef206d420d12ec3e4d94a65ee7d4881
Corey Yates <cqyates@gmail.com>
2023-12-05 22:48:17 -0500

    added carousel

---
+++ vite-project/src/components/ProjectCard/index.jsx
@@ -0,0 +1,11 @@
+import Card from "react-bootstrap/Card";
+
+const ProjectCard=()=> {
+  return (
+    <Card>
+      <Card.Title>Placeholder</Card.Title>
+    </Card>
+  )
+}
+
+export default ProjectCard;
--- vite-project/src/pages/Portfolio/index.jsx
+++ vite-project/src/pages/Portfolio/index.jsx
@@ -1,17 +1,15 @@
+
+import Carousel from "../../components/Carousel"
 const PortfolioPage = () => {
-  const projects = [
-    {
-      id: 1,
-      title: "React Image Finder",
-      description: "A react front end application with images pulled from the unsplash api",
-      tech: ["React", "RESTful APIs"],
-      website_url: "https://cqyates.github.io/react-image-finder/",
-      github_url: "https://github.com/cqyates/react-image-finder",
-      imgSrc: ""
-    }
-  ]
+
   return (
+    <>
       <h3>Portfolio Page</h3>
+      <div id="portolio-container" style={{border: "3px solid black"}}>
+      <Carousel/>
+      </div>
+        
+    </>
   )
 }
 
