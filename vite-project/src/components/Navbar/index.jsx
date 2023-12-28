833415b1f6464ab7efa2e7bd62a8e7156de712b3
Corey Yates <cqyates@gmail.com>
2023-12-27 22:47:25 -0500

    added fonts and color scheme

--- vite-project/src/components/Navbar/index.jsx
+++ vite-project/src/components/Navbar/index.jsx
@@ -1,29 +1,30 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import "./style.css"
 import { Link } from 'react-router-dom';
 const HeaderNav = () => {
   return (
-    <Navbar expand="md">
+    <Navbar className="navbar" expand="md">
       <Container fluid>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse id="navbar" className="d-flex flex-row">
-          <Nav className="ml-auto d-flex" style={{border: "1px solid pink", width: "100%", justifyContent: "space-around"}}>
+          <Nav className="ml-auto d-flex" style={{ width: "100%", justifyContent: "space-around"}}>
   
-              <Nav.Link as={Link} to="/">
+              <Nav.Link className="nav-link" as={Link} to="/">
                 About Me
               </Nav.Link>
-              <Nav.Link as={Link} to="/current">
+              <Nav.Link className="nav-link" as={Link} to="/current">
                 Current Projects
               </Nav.Link>
-              <Nav.Link as={Link} to="/skills">
+              <Nav.Link className="nav-link" as={Link} to="/skills">
                 Technical Skills and Certifications
               </Nav.Link>
-              <Nav.Link as={Link} to="/students">
+              <Nav.Link className="nav-link" as={Link} to="/students">
                My Featured Students
               </Nav.Link>
-              <Nav.Link as={Link} to="/portfolio">
+              <Nav.Link className="nav-link" as={Link} to="/portfolio">
                 My Portfolio
               </Nav.Link>
-              <Nav.Link as={Link} to="/resume">
+              <Nav.Link className="nav-link" as={Link} to="/resume">
                 Resume
               </Nav.Link>
           </Nav>
